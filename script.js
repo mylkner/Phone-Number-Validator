2a08c5185174a61c1c674feec4a725900b6e7a8e
mylkner <mylofaulkner27@gmail.com>
2024-03-12 16:30:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+const userInput = document.getElementById("user-input");
+const results = document.getElementById("results-div");
+
+function inputCheck(input) {
+    input = String(userInput.value);
+
+    if (input === "") {
+        alert("Please provide a phone number");
+        return;
+    }
+
+    const message = document.createElement("div");
+    message.className = "message";
+    message.textContent = check(input);
+    message.style.color = color(message.textContent);
+    results.appendChild(message);
+
+    userInput.value = "";
+}
+
+function check(input) {
+    const phoneRegex =
+        /^(1\s?)?(\((\d{3})\)|(\d{3}))[-\s]?(\d{3})[-\s]?(\d{4})$/;
+
+    return phoneRegex.test(input)
+        ? `Valid US number: ${input}`
+        : `Invalid US number: ${input}`;
+}
+
+function color(text) {
+    return text.split(" ")[0] === "Valid" ? "green" : "red";
+}
+
+checkBtn.addEventListener("click", inputCheck);
+
+userInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        inputCheck();
+    }
+});
+
+clearBtn.addEventListener("click", () => {
+    results.textContent = "";
+});
